706b0549a9cb077b2a7efd508332ed50be7e39ee
BigFaceHL <1286743738@qq.com>
2019-03-12 18:55:00 +0800

    test test

--- music_163/artists.py
+++ music_163/artists.py
@@ -2,16 +2,17 @@
 获取所有的歌手信息
 """
 import requests
+import urllib3
 from bs4 import BeautifulSoup
 from music_163 import sql
 
-headers = {
+cookies = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
-    'Cookie': '_ntes_nnid=7eced19b27ffae35dad3f8f2bf5885cd,1476521011210; _ntes_nuid=7eced19b27ffae35dad3f8f2bf5885cd; usertrack=c+5+hlgB7TgnsAmACnXtAg==; Province=025; City=025; NTES_PASSPORT=6n9ihXhbWKPi8yAqG.i2kETSCRa.ug06Txh8EMrrRsliVQXFV_orx5HffqhQjuGHkNQrLOIRLLotGohL9s10wcYSPiQfI2wiPacKlJ3nYAXgM; P_INFO=hourui93@163.com|1476523293|1|study|11&12|jis&1476511733&mail163#jis&320100#10#0#0|151889&0|g37_client_check&mailsettings&mail163&study&blog|hourui93@163.com; NTES_SESS=Fa2uk.YZsGoj59AgD6tRjTXGaJ8_1_4YvGfXUkS7C1NwtMe.tG1Vzr255TXM6yj2mKqTZzqFtoEKQrgewi9ZK60ylIqq5puaG6QIaNQ7EK5MTcRgHLOhqttDHfaI_vsBzB4bibfamzx1.fhlpqZh_FcnXUYQFw5F5KIBUmGJg7xdasvGf_EgfICWV; S_INFO=1476597594|1|0&80##|hourui93; NETEASE_AUTH_SOURCE=space; NETEASE_AUTH_USERNAME=hourui93; _ga=GA1.2.1405085820.1476521280; JSESSIONID-WYYY=cbd082d2ce2cffbcd5c085d8bf565a95aee3173ddbbb00bfa270950f93f1d8bb4cb55a56a4049fa8c828373f630c78f4a43d6c3d252c4c44f44b098a9434a7d8fc110670a6e1e9af992c78092936b1e19351435ecff76a181993780035547fa5241a5afb96e8c665182d0d5b911663281967d675ff2658015887a94b3ee1575fa1956a5a%3A1476607977016; _iuqxldmzr_=25; __utma=94650624.1038096298.1476521011.1476595468.1476606177.8; __utmb=94650624.20.10.1476606177; __utmc=94650624; __utmz=94650624.1476521011.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
+    'Cookie': '_ntes_nnid=fa9c8e543e36dd502ae9e6a226d4672f,1551751983384; _ntes_nuid=fa9c8e543e36dd502ae9e6a226d4672f; _ga=GA1.2.1199911014.1551751984; JSESSIONID-WYYY=Ao7VM8BP6rqM2cDCYo1iJGDdBJbvhy1ED%5CP%2FqoH7HvcUUPfp371GiSZY3DC2DBKUDRXHamMPCnvQNIedsxBm3T%5CeBGXNtFkyYYMTyu6ckHKZX7o4RJoJlwAllS4wOYaGEzexwTXMv7UD1i%2B1j%2BKZkD9B8T3lSTC%5Cfut2XT7ONvij82j0%3A1551867101251; _iuqxldmzr_=32; WM_NI=0EKDxL8xiD2HIKJQ7ai1Zxv84aGe9cq3HNuLnXbBCh%2BFmPB02zUGxYiMb%2FeZeZQrZT0skPN0uQJHPO1r%2Be4uHg2yZTedr5rZhzkjSjGhuFUW%2BfbufX3Sx1%2B5x9%2Bjfor%2Fb08%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6eed9dc6e969699dadc25a18e8aa2c15e978f8faaf27dab968686c852acf08395d32af0fea7c3b92af49b85a5fb48fca8a7a5b1448b9586b2b66babef9699f534f8bf8986bc69b3ed88aff848ae88aea3c264e99aa3b1c13cf2a69ba8fb53ab93aed0e852a7869f93d960f4ada5b1cd3cb78f86a9d345f3aee586b77f8cb9fe9ad960a7bcbed6f152aca6a58bd26285b8bba7d85de9baae96e554f18db8d2aa3fbce7e58be565b2f1aed1bb37e2a3; WM_TID=rI6kBwyYWb1BVUBEBBIohiZZeUdlldoL',
     'DNT': '1',
     'Host': 'music.163.com',
     'Pragma': 'no-cache',
@@ -20,13 +21,23 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
 }
 
+def getUrl(type,url ,params):
+    requests.packages.urllib3.disable_warnings()
+    httprequuest = urllib3.PoolManager();
+    httprespon = httprequuest.request(type, url,headers=cookies,fields=params)
+    return httprespon.data.decode()
+
+
+
+
+
 
 def save_artist(group_id, initial):
     params = {'id': group_id, 'initial': initial}
-    r = requests.get('http://music.163.com/discover/artist/cat', params=params)
+    content = getUrl('get','http://music.163.com/discover/artist/cat',params)
 
     # 网页解析
-    soup = BeautifulSoup(r.content.decode(), 'html.parser')
+    soup = BeautifulSoup(content, 'html.parser')
     body = soup.body
 
     hot_artists = body.find_all('a', attrs={'class': 'msk'})
@@ -36,7 +47,8 @@ def save_artist(group_id, initial):
         artist_id = artist['href'].replace('/artist?id=', '').strip()
         artist_name = artist['title'].replace('的音乐', '')
         try:
-            sql.insert_artist(artist_id, artist_name)
+            print(artist_id+"-hot-"+artist_name)
+            # sql.insert_artist(artist_id, artist_name)
         except Exception as e:
             # 打印错误日志
             print(e)
@@ -45,14 +57,15 @@ def save_artist(group_id, initial):
         artist_id = artist['href'].replace('/artist?id=', '').strip()
         artist_name = artist['title'].replace('的音乐', '')
         try:
-            sql.insert_artist(artist_id, artist_name)
+            print(artist_id + "--" + artist_name)
+            # sql.insert_artist(artist_id, artist_name)
         except Exception as e:
             # 打印错误日志
             print(e)
 
 
-gg = 4003
 
-save_artist(gg, 0)
-for i in range(65, 91):
-    save_artist(gg, i)
+# for i in range(65, 91):
+#     save_artist(1001, i)
+if __name__ == '__main__':
+    save_artist(1001,-1)
